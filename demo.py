26778468135f54b4fc200d74fe90bae2c77fd736
ProphetKing <wxywyyx@163.com>
2021-06-17 21:31:53 +0800

    test

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
 import numpy as np
 print("demo prophet")
 
+print("vscode://vscode.github-authentication/did-authenticate?windowid=1&code=f6d5a3ee21743fd6a74a&state=a8ad5991-0b09-4c72-b3f4-feee9a7d52bb")
